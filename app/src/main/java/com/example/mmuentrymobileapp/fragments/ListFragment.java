9e0d2d9e9d061521df1229287ca1419ec7956918
stevenkok1225@gmail.com <stevenkok1225@gmail.com>
2023-05-18 01:13:38 +0800

    update list view

--- app/src/main/java/com/example/mmuentrymobileapp/fragments/ListFragment.java
+++ app/src/main/java/com/example/mmuentrymobileapp/fragments/ListFragment.java
@@ -1,5 +1,7 @@
 package com.example.mmuentrymobileapp.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +9,63 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.example.mmuentrymobileapp.R;
+import com.example.mmuentrymobileapp.TestActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListFragment extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        SearchView searchview = view.findViewById(R.id.searchview);
+        ListView listView = view.findViewById(R.id.listview);
+
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Orange");
+        list.add("Banana");
+        list.add("Grapes");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    // clicked apple
+                    startActivity(new Intent(requireContext(), TestActivity.class));
+                } else {
+                    // Handle other item clicks
+                }
+            }
+        });
+
+        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                arrayAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return view;
     }
 }

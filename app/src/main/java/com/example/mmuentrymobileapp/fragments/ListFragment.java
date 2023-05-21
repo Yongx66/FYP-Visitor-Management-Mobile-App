612c80e96c9c1a2794c638deffe21250e5fd4ab2
stevenkok1225@gmail.com <stevenkok1225@gmail.com>
2023-05-21 20:08:49 +0800

    fix list and setting fragment bug

--- app/src/main/java/com/example/mmuentrymobileapp/fragments/ListFragment.java
+++ app/src/main/java/com/example/mmuentrymobileapp/fragments/ListFragment.java
@@ -3,16 +3,18 @@ package com.example.mmuentrymobileapp.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.SearchView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mmuentrymobileapp.R;
 import com.example.mmuentrymobileapp.TestActivity;
@@ -22,14 +24,15 @@ import java.util.List;
 
 public class ListFragment extends Fragment {
 
+    private RecyclerViewAdapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
         SearchView searchview = view.findViewById(R.id.searchview);
-        ListView listView = view.findViewById(R.id.listview);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
 
         List<String> list = new ArrayList<>();
         list.add("Apple");
@@ -37,35 +40,109 @@ public class ListFragment extends Fragment {
         list.add("Banana");
         list.add("Grapes");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(arrayAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    // clicked apple
-                    startActivity(new Intent(requireContext(), TestActivity.class));
-                } else {
-                    // Handle other item clicks
-                }
-            }
-        });
+        adapter = new RecyclerViewAdapter(list, requireContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
 
         searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                arrayAdapter.getFilter().filter(query);
+                adapter.getFilter().filter(query);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                arrayAdapter.getFilter().filter(newText);
+                adapter.getFilter().filter(newText);
                 return false;
             }
         });
 
         return view;
+    }
+
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> implements Filterable {
+
+        private List<String> itemList;
+        private List<String> filteredList;
+        private Context context;
+        private ItemFilter itemFilter;
+
+        public RecyclerViewAdapter(List<String> itemList, Context context) {
+            this.itemList = itemList;
+            this.filteredList = new ArrayList<>(itemList);
+            this.context = context;
+            this.itemFilter = new ItemFilter();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                textView = itemView.findViewById(android.R.id.text1);
+            }
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            String item = filteredList.get(position);
+            holder.textView.setText(item);
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = holder.getAdapterPosition();
+                    if (position == 0) {
+                        // clicked apple
+                        context.startActivity(new Intent(context, TestActivity.class));
+                    } else {
+                        // Handle other item clicks
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return filteredList.size();
+        }
+
+        @Override
+        public Filter getFilter() {
+            return itemFilter;
+        }
+
+        private class ItemFilter extends Filter {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String query = constraint.toString().toLowerCase();
+
+                List<String> filteredItems = new ArrayList<>();
+                for (String item : itemList) {
+                    if (item.toLowerCase().contains(query)) {
+                        filteredItems.add(item);
+                    }
+                }
+
+                FilterResults results = new FilterResults();
+                results.values = filteredItems;
+                results.count = filteredItems.size();
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                filteredList = (List<String>) results.values;
+                notifyDataSetChanged();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mmuentrymobileapp/fragments/SettingFragment.java
+++ app/src/main/java/com/example/mmuentrymobileapp/fragments/SettingFragment.java
@@ -1,10 +1,8 @@
 package com.example.mmuentrymobileapp.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +10,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.fragment.app.Fragment;
+
+import com.example.mmuentrymobileapp.MainActivity;
 import com.example.mmuentrymobileapp.QrActivity;
 import com.example.mmuentrymobileapp.R;
 import com.example.mmuentrymobileapp.TestActivity;
@@ -21,11 +22,9 @@ import java.util.List;
 
 public class SettingFragment extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
 
         ListView settingView = view.findViewById(R.id.settingview);
@@ -34,20 +33,29 @@ public class SettingFragment extends Fragment {
         list.add("Scan Qr Code");
         list.add("Logout");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, list);
-        settingView.setAdapter(arrayAdapter);
-
-        settingView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, list) {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    // clicked scan qr
-                    startActivity(new Intent(requireContext(), QrActivity.class));
-                } else {
-                    // Handle other item clicks
-                }
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (position == 0) {
+                            // clicked scan qr
+                            startActivity(new Intent(requireContext(), QrActivity.class));
+                        } else if (position == 1) {
+                            // clicked Logout
+                            startActivity(new Intent(requireContext(), MainActivity.class));
+                        }
+                    }
+                });
+
+                return view;
             }
-        });
+        };
+
+        settingView.setAdapter(arrayAdapter);
 
         return view;
     }
